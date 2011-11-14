dca86cb8257df7a97f61fa8df9c0e3028646e1a2
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-11-14 23:28:02 +0100

    Minor edits
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- gen-prog.sh
+++ gen-prog.sh
@@ -3,3 +3,6 @@ pdf90 --suffix 'rotated' prog-contents.pdf
 pdfjoin --outfile prog-a4.pdf prog-cover.pdf prog-contents-rotated.pdf prog-final.pdf
 pdfjam --a4paper --landscape --outfile prog-booklet.pdf --nup 2x1 prog-a4.pdf '8,1,2,7,6,3,4,5'
 rm -f prog-contents-rotated.pdf
+
+pdfjoin --outfile questionnaire-double.pdf questionnaire-programme.pdf questionnaire-programme.pdf
+pdfjam --a4paper --landscape --outfile questionnaire-dup.pdf --nup 2x1 questionnaire-double.pdf '1,2'
